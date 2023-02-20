b0cdf7d3b689b4b873b2340639bd9f6b108432ea
dallenpyrah <dallenpyrah@gmail.com>
2023-02-19 17:55:53 -0700

    Packaged for macos, executable is working node.js 16+

--- prisma/migrations/20230218013535_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "FrameworkOptions" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "value" TEXT NOT NULL,
-    "hint" TEXT NOT NULL,
-    "label" TEXT NOT NULL
-);
